41bd6cdc7d64aa2d8c7f379ecccdd15e6d4a047e
SanandhKumarM <sanandh.m@nineleaps.com>
2025-03-06 11:59:43 +0530

    Updated config files

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,12 +2,19 @@ import "dotenv/config";
 
 const config = {
   openai: {
-    apiKey: process.env.OPENAI_API_KEY,
-    gpt4Model: "gpt-4",
-    gpt4MiniModel: "gpt-4o-mini",
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    GPT4o: "gpt-4",
+    GPT4oMINI: "gpt-4o-mini",
   },
   server: {
-    port: process.env.PORT || 5000,
+    PORT: process.env.PORT || 5000,
+  },
+  mongo: {
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`,
+    COLLECTION_NAMES: {
+      mp_customers: "mp_customers",
+      mp_tasks: "mp_tasks",
+    },
   },
   sql: {
     DB_HOST: process.env.DB_HOST,
@@ -15,7 +22,18 @@ const config = {
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME,
   },
-  systemPrompt: {},
+  zilliz: {
+    ZILLIZ_URI: process.env.ZILLIZ_URI,
+    ZILLIZ_TOKEN: process.env.ZILLIZ_TOKEN,
+    COLLECTION_NAMES: {
+      mp_vector_store: "magic_paints_with_summary",
+    },
+  },
+  whatsApp: {
+    WHATSAPP_TOKEN: process.env.WHATSAPP_TOKEN,
+    MY_WHATSAPP_TOKEN: process.env.MY_WHATSAPP_TOKEN,
+    WHATSAPP_ACCOUNT_ID: process.env.WHATSAPP_ACCOUNT_ID,
+  },
 };
 
 export default config;
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -15,14 +15,8 @@ const router = express.Router();
 const sqlInstance = await SQLDatabase.createPool();
 
 // Initialize services
-const queryInterpreter = new QueryInterpreter();
-const taskManager = new TaskManager(sqlInstance);
-const orchestrator = new Orchestrator(queryInterpreter, taskManager);
-const scheduler = new Scheduler(orchestrator);
 
 // Initialize controllers
-const queryController = new QueryController(orchestrator);
-const schedulerController = new SchedulerController(scheduler);
 const authController = new AuthController(sqlInstance);
 
 // Authentication routes
@@ -30,28 +24,7 @@ router.post("/auth/login", (req, res) => authController.login(req, res));
 router.post("/auth/logout", (req, res) => authController.logout(req, res));
 
 // Query routes
-router.post("/query", (req, res) => queryController.submitQuery(req, res));
-router.get("/workflow/:taskId", (req, res) =>
-  queryController.getWorkflowStatus(req, res)
-);
 
 // Scheduler routes
-router.post("/schedule", (req, res) =>
-  schedulerController.scheduleQuery(req, res)
-);
-
-router.get(
-  "/schedule/:scheduleId",
-
-  (req, res) => schedulerController.getScheduledTask(req, res)
-);
-router.get("/schedules", (req, res) =>
-  schedulerController.listScheduledTasks(req, res)
-);
-router.delete(
-  "/schedule/:scheduleId",
-
-  (req, res) => schedulerController.cancelScheduledTask(req, res)
-);
 
 export default router;
