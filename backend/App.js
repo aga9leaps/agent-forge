2d641c6073d38f5f86d5e95990284931f7961df7
SanandhKumarM <sanandh.m@nineleaps.com>
2025-04-17 13:11:06 +0530

    feat: initialize Express application with routing and database setup

---
+++ backend/App.js
@@ -0,0 +1,30 @@
+import express from "express";
+import ConfigLoader from "./core/ConfigLoader.js";
+import initializeDatabases from "./core/utils/initializeDatabases.js";
+import createAgentRouter from "./src/routes/agentRouter.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+let clientConfig;
+
+async function initializeApplication() {
+  try {
+    clientConfig = await ConfigLoader.loadClientConfig("magic_paints");
+
+    await initializeDatabases(clientConfig?.databases.sql.dbName);
+
+    console.log("Application initialized successfully.");
+  } catch (error) {
+    console.error("Error initializing application:", error);
+    process.exit(1);
+  }
+}
+
+await initializeApplication();
+
+app.use(express.json());
+app.use("/api", createAgentRouter(clientConfig));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
